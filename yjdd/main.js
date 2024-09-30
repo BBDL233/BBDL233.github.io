1c212eff8e552226d614047e068438ad8d7b0557
BBDL <99265367+BBDL233@users.noreply.github.com>
2024-09-30 17:38:50 +0800

    Add files via upload

---
+++ yjdd/main.js
@@ -0,0 +1,24 @@
+if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+    window.location = "mobile-cpdd.html";
+}
+
+var header = document.querySelector('header');
+var backToTopButton = document.getElementById('backToTop');
+
+// 当用户滚动页面时执行函数
+window.onscroll = function() {
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var headerHeight = header.offsetHeight;
+
+  if (scrollPosition > headerHeight) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+};
+
+// 当用户点击按钮时，平滑滚动到页面顶部
+backToTopButton.onclick = function() {
+  // 兼容写法
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
